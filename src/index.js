7dfe82735613fadb33f2a4ee5cb676598e31c2d4
Cody Reed <cody2j2reed@gmail.com>
2019-02-10 12:45:25 -0500

    Add semantic ui cdn for styling and created components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import CommentDetail from "./components/CommentDetail";
+
 const App = () => {
-  return <div>JSX App</div>;
+  return (
+    <div className="ui container comments">
+      <CommentDetail
+        author="Cody"
+        timeAgo="Today at 4:45PM"
+        userComment="Nice Post!"
+      />
+      <CommentDetail
+        author="Scarlett"
+        timeAgo="Today at 4:45PM"
+        userComment="This isn't to bad."
+      />
+      <CommentDetail
+        author="Indigo"
+        timeAgo="Yesterday at 5:00PM"
+        userComment="Bah Bah!"
+      />
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
